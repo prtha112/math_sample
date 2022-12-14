de52f288313ee6e32eb259aaa4fabdf38b76a186
Sathabhron <thahi5_vi@hotmail.com>
2022-12-14 20:37:57 +0700

    Update variance.py

--- variance.py
+++ variance.py
@@ -1,10 +1,7 @@
 number = [600,470,170,430,300] 
 
 def variance(number):
-    sum_number = 0
-    for _number in number:
-        sum_number = sum_number + _number
-    mean_number = sum_number/len(number)
+    mean_number = sum(number)/len(number)
     
     sum_diff = []
     for _number in number:
