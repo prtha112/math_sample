53aaf3159ea62222b0db757ccaa6d1923d3955c8
Sathabhron <thahi5_vi@hotmail.com>
2022-12-14 20:41:04 +0700

    Create standard_deviation.py

---
+++ standard_deviation.py
@@ -0,0 +1,20 @@
+import math
+number = [600,470,170,430,300] 
+
+def variance(number):
+    mean_number = sum(number)/len(number)
+    
+    sum_diff = []
+    for _number in number:
+        diff = (_number - mean_number)
+        diff_pow2 = diff ** 2
+        sum_diff.append(diff_pow2)
+        
+    sum_variance = sum(sum_diff)/len(sum_diff)
+    return sum_variance
+    
+def standardDeviation(number):
+    return math.sqrt(number)
+    
+print(variance(number))
+print(standardDeviation(variance(number)))
