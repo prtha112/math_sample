1b8e46895cf77bce3180309f7aa97fe3cdba11ae
Sathabhron <thahi5_vi@hotmail.com>
2022-12-14 20:50:41 +0700

    Update standard_deviation.py

--- standard_deviation.py
+++ standard_deviation.py
@@ -1,21 +1,26 @@
 # Link : https://www.mathsisfun.com/data/standard-deviation.html
 number = [600,470,170,430,300] 
 
-def variance(number):
-    mean_number = sum(number)/len(number)
+def variance(number, population):
+    if population == True:
+        pop = 1
+    else:
+        pop =0
     
+    mean_number = sum(number)/len(number)
     sum_diff = []
+    
     for _number in number:
         diff = (_number - mean_number)
         diff_pow2 = diff ** 2
         sum_diff.append(diff_pow2)
         
-    sum_variance = sum(sum_diff)/len(sum_diff)
+    sum_variance = sum(sum_diff)/(len(sum_diff) - pop)
     return sum_variance
     
 def standardDeviation(number):
     # square root
     return number**(1/2)
     
-print(variance(number))
-print(standardDeviation(variance(number)))
+print(variance(number, True))
+print(standardDeviation(variance(number, True)))
